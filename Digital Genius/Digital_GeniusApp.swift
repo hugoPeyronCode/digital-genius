520d9bbf33b5e6d3393172f43e2355a38be2683b
Hugo Peyron <hugopeyron@gmail.com>
2023-03-17 11:33:47 +0100

    Initial Commit

---
+++ Digital Genius/Digital_GeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_GeniusApp.swift
+//  Digital Genius
+//
+//  Created by Hugo Peyron on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Digital_GeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
